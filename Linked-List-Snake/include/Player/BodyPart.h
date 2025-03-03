19b002d740fbab20d3aaddabbf6ce156ca7f89b3
ash02git <ashwinabraham2001@gmail.com>
2025-03-03 16:39:24 +0530

    Added body part.cpp

--- Linked-List-Snake/include/Player/BodyPart.h
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -6,10 +6,22 @@ namespace Player
 {
 	class BodyPart
 	{
+	private:
 		UI::UIElement::ImageView* bodypart_image;
 		sf::Vector2i grid_position;
 		Direction direction;
 		float bodypart_width;
 		float bodypart_height;
+
+		void createBodyPartImage();
+		void initializeBodyPartImage();
+		sf::Vector2f getBodyPartScreenPosition();
+	public:
+		BodyPart();
+		~BodyPart();
+
+		void initialize(float width, float height, sf::Vector2i pos, Direction dir);
+		void updatePosition();
+		void render();
 	};
 }
---
+++ Linked-List-Snake/source/Player/BodyPart.cpp
@@ -0,0 +1,48 @@
+#include "../../include/Player/BodyPart.h"
+#include "../../include/Global/Config.h"
+#include "../../include/Level/LevelView.h"
+
+namespace Player
+{
+	
+	BodyPart::BodyPart()
+	{
+		bodypart_image = nullptr;
+		createBodyPartImage();
+
+		grid_position = sf::Vector2i(0, 0);
+	}
+	void BodyPart::createBodyPartImage()
+	{
+		bodypart_image = new UI::UIElement::ImageView();
+	}
+	void BodyPart::initializeBodyPartImage()
+	{
+		bodypart_image->initialize(Global::Config::snake_body_texture_path, bodypart_width, bodypart_height, getBodyPartScreenPosition());
+		bodypart_image->setOriginAtCentre();
+	}
+	sf::Vector2f BodyPart::getBodyPartScreenPosition()
+	{
+		float x_screen_position = Level::LevelView::border_offset_left + (grid_position.x * bodypart_width) + (bodypart_width / 2);
+		float y_screen_position = Level::LevelView::border_offset_top + (grid_position.y * bodypart_height) + (bodypart_height / 2);
+
+		return sf::Vector2f(x_screen_position, y_screen_position);
+	}
+	BodyPart::~BodyPart()
+	{
+		delete(bodypart_image);
+	}
+	void BodyPart::initialize(float width, float height, sf::Vector2i pos, Direction dir)
+	{
+		bodypart_width = width;
+		bodypart_height = height;
+		direction = dir;
+		grid_position = pos;
+
+		initializeBodyPartImage();
+	}
+	void BodyPart::render()
+	{
+		bodypart_image->render();
+	}
+}
